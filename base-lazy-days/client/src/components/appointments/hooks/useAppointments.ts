32bb68ce607d22275ebebba2e3fee8ec372969c4
Fabio Posada <xFposada@teichert.com>
2022-07-16 15:14:48 -0500

    section 5

--- base-lazy-days/client/src/components/appointments/hooks/useAppointments.ts
+++ base-lazy-days/client/src/components/appointments/hooks/useAppointments.ts
@@ -1,6 +1,7 @@
 // @ts-nocheck
 import dayjs from 'dayjs';
-import { Dispatch, SetStateAction, useState } from 'react';
+import { Dispatch, SetStateAction, useEffect, useState } from 'react';
+import { useQuery, useQueryClient } from 'react-query';
 
 import { axiosInstance } from '../../../axiosInstance';
 import { queryKeys } from '../../../react-query/constants';
@@ -63,14 +64,23 @@ export function useAppointments(): UseAppointments {
   /** ****************** START 3: useQuery  ***************************** */
   // useQuery call for appointments for the current monthYear
 
-  // TODO: update with useQuery!
-  // Notes:
-  //    1. appointments is an AppointmentDateMap (object with days of month
-  //       as properties, and arrays of appointments for that day as values)
-  //
-  //    2. The getAppointments query function needs monthYear.year and
-  //       monthYear.month
-  const appointments = {};
+  const queryClient = useQueryClient();
+  const fallback = {};
+  const { data: appointments = fallback } = useQuery(
+    [queryKeys.appointments, monthYear.year, monthYear.month],
+    () => getAppointments(monthYear.year, monthYear.month),
+  );
+
+  useEffect(() => {
+    if (monthYear?.month) {
+      const nextMonth = getNewMonthYear(monthYear, 1);
+      queryClient.prefetchQuery(
+        [queryKeys.appointments, monthYear.year, nextMonth.month],
+        () => getAppointments(monthYear.year, nextMonth.month),
+      );
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [monthYear.month]);
 
   /** ****************** END 3: useQuery  ******************************* */
 
