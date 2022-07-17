54e3fc561dc6269b6201d9f0feee980d43a8401e
Fabio Posada <xFposada@teichert.com>
2022-07-17 17:06:50 -0500

    lesson 61

--- base-lazy-days/client/src/components/user/hooks/useUser.ts
+++ base-lazy-days/client/src/components/user/hooks/useUser.ts
@@ -45,6 +45,7 @@ export function useUser(): UseUser {
   // meant to be called from useAuth
   function clearUser() {
     queryClient.setQueryData(queryKeys.user, null);
+    queryClient.removeQueries(queryKeys.userAppointments);
   }
 
   return { user, updateUser, clearUser };
